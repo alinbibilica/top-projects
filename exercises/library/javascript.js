4eacb79f26b1210ae70b8c838289bfd7c4345524
alinbibilica <alinconstantinbb@gmail.com>
2025-12-04 23:59:15 +0200

    issue: doubles div when adding books

--- exercises/library/javascript.js
+++ exercises/library/javascript.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 const myLibrary = [];
 
 const library1 = document.getElementById("library");  //main container to display library books
@@ -10,7 +5,8 @@ const library1 = document.getElementById("library");  //main container to displa
 const addButton = document.getElementById('addBookButton'); // Add Book Button used to open the "add book form"
 const closeButton = document.getElementById('closeButton'); // button to close the "add book form" if user decides not to add a new book
 const addBookForm = document.getElementById('addBookForm'); // Add Book Form where users inputs his book details to add them to the library
-
+const submitBookButton = document.getElementById('submitNewBook'); // Add New Book Submit button
+ 
 addButton.addEventListener("click", function(e){
     addBookForm.style.display = "grid";
 });
@@ -25,50 +21,48 @@ function Book(title, author, nrOfPages ){
         }
         this.title = title,
         this.author = author
-        this.nrOfPages = nrOfPages;
-         
+        this.nrOfPages = nrOfPages; 
+        
 }
 
 Book.prototype.getId = function(){
     this.idNr = crypto.randomUUID();
 }
 
+Book.prototype.getStatus = function(){
+    this.status = bookStatus;
+}
  
 function addBookToLibrary(title, author, noOfPages){
     const addBook = new Book(title, author, noOfPages);
     addBook.getId();
+    addBook.getStatus();
     myLibrary.push(addBook);
+    addBookForm.style.display = "none";
 }
 
-addBookToLibrary('main theme', 'jackson', 5);
-addBookToLibrary('the covenent', 'winon rider', 522);
-addBookToLibrary('main theme', 'jackson', 5);
-addBookToLibrary('the covenent', 'winon rider', 522);
-addBookToLibrary('main theme', 'jackson', 5);
-addBookToLibrary('the covenent', 'winon rider', 522);
-addBookToLibrary('main theme', 'jackson', 5);
-
-
-
-
+let libraryContainer = document.getElementById('library');
 //functie care adauga elementele in DOM
-
 function addElements(){
-
-    let libraryContainer = document.getElementById('library');
+    
+    
 
     myLibrary.forEach((item)=>{
+
+        
         let bookTitle = document.createElement('div');
         let bookAuthor = document.createElement('div');
         let bookPages = document.createElement('div');
         let booksCard = document.createElement('div');
         let bookId = document.createElement('div');
+        let bookStatus = document.createElement('div');
         let bookRemoveButton = document.createElement('button');
 
 
-        bookTitle.innerText = `Title: "${item.title}"`;
-        bookAuthor.innerText = ` Written by: "${item.author}".`;
-        bookPages.innerText = `Pages: ${item.nrOfPages}`;
+        bookTitle.innerText = `Title: "${item.title.value}"`;
+        bookAuthor.innerText = ` Written by: "${item.author.value}".`;
+        bookPages.innerText = `Pages: ${item.nrOfPages.value}`;
+        bookStatus.innerText = `Status: ${item.status.value}`;
         bookId.innerText = `Book Id: ${item.idNr}`;
         bookRemoveButton.innerText = `Remove`;
 
@@ -76,18 +70,49 @@ function addElements(){
         booksCard.appendChild(bookAuthor);
         booksCard.appendChild(bookPages);
         booksCard.appendChild(bookId);
+        booksCard.appendChild(bookStatus);
         booksCard.appendChild(bookRemoveButton);
 
         booksCard.classList.add("books");
         
-        libraryContainer.appendChild(booksCard);
+         libraryContainer.appendChild(booksCard);
     }
-
     )
    
-
  };
 
- addElements();
+
+submitBookButton.addEventListener("click", function(e){
+    console.log("submit pressed");
+
+    const newBookTitle = document.getElementById('bookTitle');
+    const newBookAuthor = document.getElementById('bookAuthor');
+    const newBookNrOfPages = document.getElementById("bookPages");
+    const newBookStatus = document.getElementById("bookStatus");
+
+    event.preventDefault();
+
+    console.log(`Book title is: ${newBookTitle.value} written by ${newBookAuthor.value} , it has ${newBookNrOfPages.value} pages and it was ${newBookStatus.value}`);
+
+    addBookToLibrary(newBookTitle, newBookAuthor, newBookNrOfPages);
+    console.log(myLibrary);
+
+    addElements();
+});
+
+
+// addBookToLibrary('main theme', 'jackson', 5);
+// addBookToLibrary('the covenent', 'winon rider', 522);
+// addBookToLibrary('main theme', 'jackson', 5);
+// addBookToLibrary('the covenent', 'winon rider', 522);
+// addBookToLibrary('main theme', 'jackson', 5);
+// addBookToLibrary('the covenent', 'winon rider', 522);
+// addBookToLibrary('main theme', 'jackson', 5);
+
+
+
+
+
+ //addElements();
 
  console.log(myLibrary)
